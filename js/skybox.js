f7d802659f5fafb5a0938dcf9060f3146c53b4cc
Master of Shapes <adamsamaral@gmail.com>
2013-09-03 13:01:13 -0700

    Basic jet ski control and skybox

---
+++ js/skybox.js
@@ -0,0 +1,17 @@
+/** Skybox Code**/
+
+
+	var imagePrefix = "images/skybox/bluesky_";
+	var directions  = ["posx", "negx", "posy", "negy", "posz", "negz"];
+	var imageSuffix = ".jpg";
+	var skyGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );	
+	
+	var materialArray = [];
+	for (var i = 0; i < 6; i++)
+		materialArray.push( new THREE.MeshBasicMaterial({
+			map: THREE.ImageUtils.loadTexture( imagePrefix + directions[i] + imageSuffix ),
+			side: THREE.BackSide
+		}));
+	var skyMaterial = new THREE.MeshFaceMaterial( materialArray );
+	var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
+	scene.add( skyBox );
